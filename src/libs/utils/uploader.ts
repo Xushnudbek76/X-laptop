e78ff69f4543990c7fbd06949122af09e7ea8546
Xushnudbek76 <abdugafforovxushnudbek17@gmail.com>
2026-06-05 22:41:25 +0900

    fix: save relative upload paths

--- src/libs/utils/uploader.ts
+++ src/libs/utils/uploader.ts
@@ -6,9 +6,10 @@ import { v4 } from "uuid";
 function getTargetImageStorage(address: any) {
   return multer.diskStorage({
     destination: function (req, file, cb) {
-      const uploadPath = path.join(process.cwd(), "uploads", address);
-      fs.mkdirSync(uploadPath, { recursive: true });
-      cb(null, uploadPath);
+      const relativeUploadPath = path.join("uploads", address);
+      const absoluteUploadPath = path.join(process.cwd(), relativeUploadPath);
+      fs.mkdirSync(absoluteUploadPath, { recursive: true });
+      cb(null, relativeUploadPath);
     },
     filename: function (req, file, cb) {
       const extension = path.parse(file.originalname).ext;
